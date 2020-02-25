315cd0d7d2fa2a870295b7804ae6dfdab341bdfa
danielzmbp <daniel.gomez-perez@zmbp.uni-tuebingen.de>
2020-02-25 14:37:15 +0100

    add pillars script
    Former-commit-id: 929aeb0fe44be830d534eb96a65fc405024e9924 [formerly c9ba0f906d97c6cddb68e047f79a41abb988f643] [formerly d9587bb3e1361ea8189eb5eba63bf5bd64edbed0 [formerly bd3ceafb7e797c1a5b83e33279508247beda3fc2]] [formerly 4bdbb12e05315138930d8947e5e5d247d9faf89c [formerly 7d251d936f1b8e2f63362657556fa282b3bb4a6c] [formerly dd03925e5eef7d9dbf06ed197c29038dbca43c95 [formerly bd2081839bf7ec11ac9ba26bbd2b4e3db464d3fd]]]
    Former-commit-id: dacc7ed77876d5d344838e0b0bb83419872a7dde [formerly 59c093cba8d1e8197deebaf3dcb50ad3a5c1bf36] [formerly 92dc8b87895f275e54d4cf7eb0e379a2be8da850 [formerly b1fee7a70f8651a9f8796d44430a04dfadd5179e]]
    Former-commit-id: ce46dbd001fc00a8864fbcec5a66be38632a09cc [formerly f70e1445dc6aac5bbc5945943c29f1d1700346e6]
    Former-commit-id: b7b167b19fa724fa833d35ffe89904d3ef768195

---
+++ scripts/pillars.py
@@ -0,0 +1,114 @@
+# coding: utf-8
+
+# ## Homology groups OGOB study
+# Use this groups to create protein families than can be alined and tested for
+# positive selection later.
+
+import pandas as pd
+import numpy as np
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+
+from Bio import codonalign
+from Bio import AlignIO
+from Bio.Alphabet import generic_dna
+from Bio.Alphabet import generic_protein
+
+poff_tsv = "fungi.poff.tsv"  # replace with your file name
+
+pillars = pd.read_csv(poff_tsv, "\t")
+
+pillars = pillars.replace("*", np.nan)
+
+pillars = pillars.iloc[:, 3:]
+
+pillars["count"] = pillars.count(1)
+
+aa_all = SeqIO.index("AA.faa", "fasta", IUPAC.protein)
+na_all = SeqIO.index("NT.fna", "fasta", IUPAC.ambiguous_dna)
+
+
+pillars["family"] = pillars.index
+
+melted = pillars.melt(["family", "count"])
+
+melted[melted["count"] > 4].dropna()[["family", "value"]].to_csv("fam.txt",
+                                                                 "\t",
+                                                                 index=False,
+                                                                 header=False)
+
+
+# # Modify fam.txt to put duplications in the same family
+
+fam = pd.read_csv("fam.txt", "\t", header=None)
+
+# separate the second column by the comma.
+
+fam_separated = fam[1].str.split(",", expand=True)
+
+
+# merge the separated dataframe with the regular one so that I get
+# the family names
+
+fam[1] = np.nan
+
+merged = fam.merge(fam_separated, left_index=True, right_index=True).melt(
+        "0_x")
+
+# replace none in dataframe
+
+merged.replace(to_replace=[None], value=np.nan, inplace=True)
+
+merged["value"].dropna().str.contains(",").value_counts()
+
+
+merged[["0_x", "value"]].dropna().to_csv("fam_dup.txt", "\t", index=False,
+                                         header=False)
+
+# # Make files for the nucleotide sequences
+
+node_file = "fam_dup.txt"
+na_file = "NT.fna"
+
+famDict = {}
+
+with open(node_file) as f:
+    for line in f:
+        row = line.split()
+        if row[0] not in famDict:
+            famDict[row[0]] = [row[1]]
+        else:
+            famDict[row[0]].append(row[1])
+
+naseqDict = SeqIO.to_dict(SeqIO.parse(na_file, "fasta"))
+
+for i in famDict.keys():
+    file = "families/family_" + i + ".fna"
+    with open(file, "w") as out:
+        for j in famDict[i]:
+            out.write('>' + j + '\n')
+            out.write(str(naseqDict[j].seq) + '\n')
+
+# # Make files for the amino acid sequences
+
+node_file = "fam_dup.txt"
+aa_file = "AA.faa"
+
+famDict = {}
+
+with open(node_file) as f:
+    for line in f:
+        row = line.split()
+        if row[0] not in famDict:
+            famDict[row[0]] = [row[1]]
+        else:
+            famDict[row[0]].append(row[1])
+
+aaseqDict = SeqIO.to_dict(SeqIO.parse(aa_file, "fasta"))
+
+for i in famDict.keys():
+    file = "families/family_" + i + ".faa"
+    with open(file, "w") as out:
+        for j in famDict[i]:
+            out.write('>' + j + '\n')
+            out.write(str(aaseqDict[j].seq) + '\n')
