7a5a39494793c6e4c586c0d32947ef2ce8703ba6
danielzmbp <daniel.gomez-perez@zmbp.uni-tuebingen.de>
2020-04-06 21:08:41 +0200

    fix scripts
    Former-commit-id: 0658596837c34f006bf5a3cea068455206fecaab [formerly 5ff5bcc2598ae7c2a29e31cfd93ae500b5703443] [formerly b73c2fe663dc9b47b9e41862478c22ccdee103e2 [formerly 03ada09ea017d851ea0df56ed67a1fc10b686533]] [formerly f2812af29aab78b602d58dd58e229297aaf94501 [formerly 9b5829951be5b331a55d6eba4861466ca7170f19] [formerly 6e8771a89ae75f2614fa329eb3c3a070326aefbe [formerly 1d455ca0223daeef4ffd6527b14a98cd62a1e75e]]]
    Former-commit-id: d0e9162e9b17e2a70894b4ae7ab55c262ed3897e [formerly 7070c2beabddcf1fda170dd0ab4a5a80e7f695e4] [formerly 938f3cb7b1d07fc4e3dd68fac87a6a6e30870f1c [formerly c1c51fee951575667517b4c390bd30b94fd7140d]]
    Former-commit-id: dc414b7e1266d563b154752b082a1d9647156553 [formerly 6c71aafd4f18522a084bd412b2911298b3e73e50]
    Former-commit-id: 498d640b92e6a3601cde5293a282725333b0f392

--- scripts/absrel.py
+++ scripts/absrel.py
@@ -1,21 +1,39 @@
 # # Remove families not in famsUnderSelection
 
 import os
-from shutil import copyfile
+from shutil import move
 import pandas as pd
+import glob
+import re
 
-fams = pd.read_csv("final_results/famsUnderAbsrel.txt", r"\s+",
+os.mkdir("final")
+output_file = "final/absrel.txt"
+
+logs = glob.glob("families_fubar/*.log")
+
+with open(output_file, "w") as out:
+    out.write("family num_SUS\n")
+    for currentFile in input.log:
+        with open(currentFile) as f:
+            for line in f:
+                if "Likelihood" in line:
+                    if "**0**" not in line:
+                        result = re.search('found(.*)branches', line)
+                        out.write(currentFile.split(
+                            "/")[-1].split(".")[0] + " " + result.group(1) + "\n")
+
+# move all positive files to new folder
+
+fams = pd.read_csv("final/absrel.txt", r"\s+",
                    index_col=False)
 
 families = fams["family"]
 
 families_in_dir = os.listdir("families_fubar")
 
-families_in_dir[2].split(".")[0]
-
 os.mkdir("families_absrel/")
 
 for i in range(0, len(families_in_dir)):
     if families_in_dir[i].split(".")[0] in list(families):
-        copyfile("families_fubar/" + families_in_dir[i], "families_absrel/" +
-                 families_in_dir[i])
+        move("families_fubar/" + families_in_dir[i],
+                 "families_absrel/" + families_in_dir[i])
--- scripts/fubar.py
+++ scripts/fubar.py
@@ -1,4 +1,4 @@
-from shutil import copyfile
+from shutil import move
 import pandas as pd
 import os
 import re
@@ -7,7 +7,7 @@ import glob
 # makes final_stats file
 
 os.mkdir("final")
-output_file = "final/final_stats.txt"
+output_file = "final/fubar.txt"
 
 logs = glob.glob("families/*.log")
 
@@ -21,20 +21,18 @@ with open(output_file, "w") as out:
                   result = re.search("inferred(.*)sites", line)
                   out.write(log.split("/")[-1].split(".")[0] + " " + result.group(1) + "\n")
 
-# copy all positive files to new folder
+# move all positive files to new folder
 
-fams = pd.read_csv("final/final_stats.txt", r"\s+",
+fams = pd.read_csv("final/fubar.txt", r"\s+",
                    index_col=False)
 
 families = fams["family"]
 
 families_in_dir = os.listdir("families")
 
-families_in_dir[2].split(".")[0]
-
 os.mkdir("families_fubar/")
 
 for i in range(0, len(families_in_dir)):
     if families_in_dir[i].split(".")[0] in list(families):
-        copyfile("families/" + families_in_dir[i],
+        move("families/" + families_in_dir[i],
                  "families_fubar/" + families_in_dir[i])
