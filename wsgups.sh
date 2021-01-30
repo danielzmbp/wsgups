241a3239f7e12c440e974c1f84929adb03d23480
danielzmbp <daniel.gomez-perez@uni-tuebingen.de>
2021-01-30 11:04:39 +0100

    fix mafft rule, add bash script
    Former-commit-id: 853e5ec524171843efc674b6c771a07133aae734
    Former-commit-id: 0a3973d853fa7e0f7609d55b3e8e4c77c64b5720
    Former-commit-id: 71e693d49778c8123c72f231f9093c39238f415b
    Former-commit-id: eb4583643c604d59ca744b97daea9ef0fdee1600 [formerly 282bc0e1bd156da19ff8c378f3307532ff4d41f8] [formerly aa831e74f1569b8db7f5229d36f0124f20b71b3a [formerly 1f63886c66956f62c2409d66a32c152c5663a53a]] [formerly c0eddea65060d6973e0b9588d8a330f3fc33bf53 [formerly 9a3b3ff60ff72045fcae8ed973443103a4a0b5c6] [formerly a727073144a41d85b735c1e6326ede443c16d3d9 [formerly eba2716f8303ef23d6202eab66dda42b0c911401]]]
    Former-commit-id: 2d58df784a2dd40f8fb1cb356df4cca3bd0db18f [formerly 4038b167a61bd30e8b48f6197746fa71bb07c149] [formerly 3e9ff5cfe7227bd743c28219dc99542ee6950a1f [formerly 8bb46800d9ba188d7a10d7307e837a91f531b440]]
    Former-commit-id: 46380046b151efa726e02eeb5d49ee1620bb984b [formerly ae68f5b8e780abb35c4d5aa5350e87950ae59c6d]
    Former-commit-id: 924c8b5a49393d9b1bd1413c0afb30005cc4e1e0

---
+++ wsgups.sh
@@ -0,0 +1,15 @@
+#PBS -l nodes=1:ppn=28
+#PBS -l walltime=48:00:00
+#PBS -S /bin/bash
+#PBS -N wsgups
+#PBS -j oe
+#PBS -o LOG
+
+# this bash script runs the pipeline in an HPC environment
+# please adapt to your particular HPC settings
+
+cd $PBS_O_WORKDIR
+
+source activate base
+
+snakemake --cluster "qsub -q short" --use-conda --local-cores 28 -j 28
